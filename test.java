eb6748f34ab86b1281061bb3aec2c83e952843ce
tkheo <gjxorud03@gmail.com>
2020-08-20 11:17:44 +0900

    2st

--- test.java
+++ test.java
@@ -1,26 +1,7 @@
 package all;
-import java.io.BufferedInputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.net.Socket;
-import java.net.UnknownHostException;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.Enumeration;
 
-import javax.crypto.Cipher;
-import javax.crypto.KeyGenerator;
-import javax.crypto.NoSuchPaddingException;
-import javax.crypto.SecretKey;
-import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpSession;
-import javax.servlet.http.HttpSessionContext;
+
 
 @SuppressWarnings("deprecation")
 public class AllDefects extends HttpServlet {
